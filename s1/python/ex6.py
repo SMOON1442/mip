54df78bd98a58d0449defe2836c77bd5ebb0387a
Mohammed BOULAHTAR <65912058+SMOON1442@users.noreply.github.com>
2024-12-09 11:37:37 +0100

    Update ex6.py

--- s1/python/ex6.py
+++ s1/python/ex6.py
@@ -1,14 +1,16 @@
 while(True):
     n=int(input("Entrez la taille du tableau : "))
-    if n > 0:
+    if n >= 2:
         break
 t=[]
-for i in range(0, n):
+for i in range(0,n):
     t.append(int(input("Entrez une valeur : ")))
-    temp = i
-    for j in range(i+1, n):
-        if temp > t[j]:
-            temp=j
-    
-    
-        
+    print(t)
+for i in range(0,n-1):
+    imin=i
+    for j in range(i+1,n):
+        if(t[j]<t[imin]):
+            imin=j
+    if imin!=i:
+        t[imin],t[i]=t[i],t[imin]
+print(t) 
